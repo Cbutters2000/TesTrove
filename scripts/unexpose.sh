8184b43492995efb198d72c0af3c8291e6a18db2
Cbutters2000 <95829442+Cbutters2000@users.noreply.github.com>
2025-07-16 11:30:05 -0600

    Create unexpose.sh

---
+++ scripts/unexpose.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ -d /mnt/TeslaCam/TeslaCam ] ; then
+ echo "Drives are already unmounted from the Tesla";
+else
+echo "Now Dismounting Drives from Tesla and making them accessible to Rpi. <BR>";
+modprobe -r g_mass_storage;
+sleep 4;
+echo "Mounting Drives to Rpi </br>";
+kpartx -a /piusb.bin;
+sleep 2;
+mount -o iocharset=utf8 -o loop /dev/mapper/loop0p2 /mnt/TeslaCam;
+mount -o iocharset=utf8 -o loop /dev/mapper/loop0p3 /mnt/TeslaMusic;
+echo "TeslaCam and TeslaMusic are now accessible to the Rpi."
+
+fi
