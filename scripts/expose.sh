cfc39e7d91a98034a212f68e1d977e8cb88a0c7a
Cbutters2000 <95829442+Cbutters2000@users.noreply.github.com>
2025-07-16 12:32:19 -0600

    Update expose.sh

--- scripts/expose.sh
+++ scripts/expose.sh
@@ -5,7 +5,7 @@ if [ -d /mnt/TeslaCam/TeslaCam ] ; then
  umount /mnt/TeslaMusic;
  kpartx -d /piusb.bin;
  echo "Initiating Drives for use by Tesla. <BR>";
- modprobe g_mass_storage file=/piusb.bin ro=0 stale=0;
+ modprobe g_mass_storage file=/piusb.bin stale=0;
  echo 'Drives Mounted. Please check for Dashcam Icon.';
 else
  echo "TeslaCam Already Mounted"
